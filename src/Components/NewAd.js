a0aaa977042980e917eabcb823b3495dca98f827
kolozsvarikrisztian <laszlokrisztiankolozsvari@gmail.com>
2023-03-16 13:49:53 +0100

    2023.03.16.

--- src/Components/NewAd.js
+++ src/Components/NewAd.js
@@ -1,10 +1,11 @@
 
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 function NewAd() {
     const [categories, setCategory] = useState([]);
-    const hibaRef = useRef(null);
+    const [hibas, setHibas] = useState("");
+    // const hibaRef = useRef(null);
     const navigate = useNavigate();
     useEffect(() => {
         fetch('http://localhost:5000/api/kategoriak', {
@@ -12,7 +13,7 @@ function NewAd() {
           })
           .then((data) => data.json())
           .then(data => setCategory(data))       
-    }, [])
+    }, []);
   return (
     <div className="container">
         <h2 className="mb-4 text-center">Új hirdetés elküldése</h2>
@@ -29,13 +30,22 @@ function NewAd() {
                 method: 'POST',
                 headers: {"content-type": "application/json"},
                 body: JSON.stringify(ujIngatlan)
+            })
+            // if (response.status === 200) {
+            //     navigate('/offers');
+            // } else {
+            //     setHibas(`${response.status} ${response.statusText}`);
+            // }
+            .then(response => {
+                if (response.status === 200) {
+                    navigate('/offers');               
+                } else {
+                    setHibas(response.statusText);
+                }
+            })
+            .catch( err => {
+                setHibas(err.message);
             });
-            if (response.status === 200) {
-                navigate('/offers');               
-            } else {
-                hibaRef.current.innerHTML = response.statusText;
-            }
-
         }}>
             <div className="offset-lg-3 offset-md-2 col-lg-6 col-md-8 col-12">
                 <div className="mb-3">
@@ -57,7 +67,7 @@ function NewAd() {
                     <textarea className="form-control" name="leiras" rows="3"></textarea>
                 </div>
                 <div className="form-check mb-3">
-                    <input className="form-check-input" type="checkbox" name="tehermentes" checked/>
+                    <input className="form-check-input" type="checkbox" name="tehermentes" defaultChecked="true"/>
                     <label className="form-check-label" for="creditFree">Tehermentes ingatlan</label>
                 </div>
                 <div className="mb-3">
@@ -68,10 +78,12 @@ function NewAd() {
                     <button className="btn btn-primary px-5" type="submit">Küldés</button>
                 </div>
 
-                <div className="alert alert-danger alert-dismissible" role="alert">
-                    <strong ref={hibaRef}>Hiba szövege</strong>
-                    <button type="button" className="btn-close"></button>
-                </div>
+                { hibas && 
+                    <div className="alert alert-danger alert-dismissible" role="alert">
+                        <strong>{hibas}</strong>
+                        <button type="button" className="btn-close" onClick={() => setHibas("")}></button>
+                    </div>
+                }
 
             </div>
         </form>
